4ab22fbdc2a632fcb0dd711ea26f5ec5ceaa1f66
Martin-Venegas-M <mvenegasm@ine.gob.cl>
2025-08-26 21:31:58 -0400

    standardize tab names

--- analysis/desc_crossectional_analysis.R
+++ analysis/desc_crossectional_analysis.R
@@ -17,21 +17,32 @@ pacman::p_load(
     haven,
     tidylog,
     rlang,
-    sjlabelled
+    sjlabelled,
+    glue
 )
 
 # 2. Load data ------------------------------------------------------------------------------------------------------------------------------------------
 
 load("input/data/proc/elsoc_proc.RData")
 
-# 3. Analysis ------------------------------------------------------------------------------------------------------------------------------
+# Declate date and user
+date <- format(Sys.Date(), "%y%m%d")
+user <- tolower(Sys.info()["user"])
 
-# 3.1 Descriptive analysis of independent variables ----------------------------------------------------------------------------------------
+# 3. Analysis -------------------------------------------------------------------------------------------------------------------------------------------
 
-# Generate vector of variables
-predictores <- elsocs[[1]] %>%
-    select(class:nse_indiv, nse_barrio, nse_barrio_norm) %>%
-    names()
+# 3.1 Descriptive analysis of independent variables -----------------------------------------------------------------------------------------------------
+
+# Generate vector of predictor variables
+preds <- c(
+    "class", "class_8", "class_5",
+    "educ", "ln_income", "quint_inc",
+    "isco", "isei",
+    "age", "age_sq", "sex",
+    "homeowner", "married", "has_children",
+    "nse_barrio", "nse_barrio_norm",
+    "pop_density", "pct_migrant", "insecurity"
+)
 
 # Function for creating tibble with stats
 create_desc_tabs <- function(data, vars, wave) {
@@ -41,19 +52,21 @@ create_desc_tabs <- function(data, vars, wave) {
         rename_with(~ (paste0(.x, wave)))
 }
 
-# Create desc tables
-elsoc_2016_desc <- create_desc_tabs(elsocs[["elsoc_2016"]], predictores, "_w01")
-elsoc_2019_desc <- create_desc_tabs(elsocs[["elsoc_2019"]], predictores, "_w04")
-elsoc_2022_desc <- create_desc_tabs(elsocs[["elsoc_2022"]], predictores, "_w06")
+# Create descriptive tables per wave
+elsocs_desc_tabs <- map2(
+    c("elsoc_2016", "elsoc_2019", "elsoc_2022"),
+    c("_w01", "_w04", "_w06"),
+    ~ create_desc_tabs(elsocs[[.x]], preds, .y)
+) %>% list_cbind()
 
 # Create function for saving tab per stat
 save_tabs <- function(stat) {
-    elsoc_desc <- bind_cols(elsoc_2016_desc, elsoc_2019_desc, elsoc_2022_desc) %>%
+    elsoc_desc <- elsocs_desc_tabs %>%
         select(variable = variable_w01, starts_with(stat))
 }
 
 # Create stats vector to iterate
-stats <- str_replace_all(names(elsoc_2016_desc), "_w01$", "")
+stats <- c("minimum", "q1", "median", "mean", "q3", "maximum", "na")
 
 # List with stats
 desc_tabs <- map(stats, ~ save_tabs(.x))
@@ -67,13 +80,14 @@ desc_tabs[["mean"]]
 desc_tabs[["q3"]]
 desc_tabs[["maximum"]]
 
-# Save
-writexl::write_xlsx(desc_tabs, "output/tables/desc_tabs_indep.xlsx")
+# Save!
+writexl::write_xlsx(desc_tabs, glue("output/tables/{date}_desc_tabs_preds.xlsx"))
+rm(preds, create_desc_tabs, elsocs_desc_tabs, save_tabs, stats, desc_tabs)
 
-# 4.2 Analysis per social calss-------------------------------------------------------------------------------------------------------------
+# 3.2 Dependent variables per social class -------------------------------------------------------------------------------------------------------------
 
 # Function for showing means of each wave per variable group
-group_analysis <- function(var_group, variable) {
+mean_per_group <- function(var_group, variable) {
     elsoc_2016 <- elsocs[["elsoc_2016"]] %>%
         mutate(!!sym(var_group) := to_label(!!sym(var_group))) %>%
         group_by(!!sym(var_group)) %>%
@@ -106,80 +120,41 @@ group_analysis <- function(var_group, variable) {
 }
 
 # Vectors to iterate
-varsdep <- elsocs[[1]] %>%
-    select(identification:justif_violence) %>%
-    names()
-
-# Re-label classes
-
-relabel_class <- function(data) {
-    # Create grouped categories of social class
-    data <- data %>%
-        mutate(
-            class_8 = set_labels(
-                class_8,
-                labels = c(
-                    "Self-employed professionals and large employers" = 1,
-                    "Small business owners" = 2,
-                    "Technical (semi-)professionals" = 3,
-                    "Production workers" = 4,
-                    "(Associate) managers" = 5,
-                    "Clerks" = 6,
-                    "Socio-cultural (semi-)professionals" = 7,
-                    "Service workers" = 8,
-                    "Retired" = 9,
-                    "Unemployed" = 10
-                )
-            ),
-            # Set labels for the five categories version of class
-            class_5 = set_labels(
-                class_5,
-                labels = c(
-                    "Higher-grade service class" = 1,
-                    "Lower-grade service class" = 2,
-                    "Small business owners" = 3,
-                    "Skilled workers" = 4,
-                    "Unskilled workers" = 5,
-                    "Retired" = 6,
-                    "Unemployed" = 7
-                )
-            )
-        )
-}
-
-elsocs <- map(elsocs, ~ relabel_class(.x))
+varsdep <- c(
+    "identification", "friends", "size_network", "size_network_rec",
+    "gen_trust", "trust_minorities", "trust_inst", "interest_pol",
+    "satisf_demo", "conv_particip", "unconv_particip", "egalitarianism",
+    "altruistic", "prosoc_behave", "democracy_support", "justif_violence"
+)
 
 # List with tabs
-group_analysis_tab_class8 <- map_df(varsdep, ~ group_analysis("class_8", .x))
-group_analysis_tab_class5 <- map_df(varsdep, ~ group_analysis("class_5", .x))
+vardep_class_8_tab <- map_df(varsdep, ~ mean_per_group("class_8", .x))
+vardep_class_5_tab <- map_df(varsdep, ~ mean_per_group("class_5", .x))
 
 # Save!
-writexl::write_xlsx(group_analysis_tab_class8, "output/tables/varsdep_class8_tab.xlsx")
-writexl::write_xlsx(group_analysis_tab_class5, "output/tables/varsdep_class5_tab.xlsx")
+writexl::write_xlsx(vardep_class_8_tab, glue("output/tables/{date}_varsdep_class8_tab.xlsx"))
+writexl::write_xlsx(vardep_class_5_tab, glue("output/tables/{date}_varsdep_class5_tab.xlsx"))
+rm(vardep_class_8_tab, vardep_class_5_tab)
 
-# 4.3 NSE barrio per varsdep --------------------------------------------------------------------------------------------------------------
+# 3.3 Dependent variables per quintiles nse neighbourhood ---------------------------------------------------------------------------------------------
 
-# Create quantiles #! TEMPORAL, LUEGO DEBO CREARLO EN EL PROCESAMIENTO.
+# Save!
+writexl::write_xlsx(vardep_quint_nse_barrio_tab, glue("output/tables/{date}_vardep_quint_nse_barrio_tab.xlsx"))
+rm(vardep_quint_nse_barrio_tab)
 
-elsocs <- map(elsocs, .f = function(x) x %>% mutate(quint_nse_barrio = ntile(nse_barrio_norm, 5)))
-
-nse_analysis_tab <- map_df(varsdep, ~ group_analysis("quint_nse_barrio", .x))
-writexl::write_xlsx(nse_analysis_tab, "output/tables/nse_analysis_tab.xlsx")
-
-# 4.4 NSE barrio per social class ---------------------------------------------------------------------------------------------------------
-
-# Create tabs class x nse barrio
-nse_barrio_class <- list(
-    group_analysis("class_8", "nse_barrio_norm"),
-    group_analysis("class_5", "nse_barrio_norm")
+# 3.4 NSE barrio per social class ---------------------------------------------------------------------------------------------------------------------
+nse_barrio_norm_class <- list(
+    mean_per_group("class_8", "nse_barrio_norm"),
+    mean_per_group("class_5", "nse_barrio_norm")
 )
-names(nse_barrio_class) <- c("class_8_nse_barrio", "class_5_nse_barrio")
+names(nse_barrio_norm_class) <- c("class_8_nse_barrio", "class_5_nse_barrio")
 
 # Save!
-writexl::write_xlsx(nse_barrio_class, "output/tables/nse_barrio_class_tab.xlsx")
+writexl::write_xlsx(nse_barrio_norm_class, glue("output/tables/{date}_nse_barrio_norm_class_tab.xlsx"))
+rm(mean_per_group, nse_barrio_norm_class, varsdep)
 
-# 4.5 Quintil NSE Barrio x Class ----------------------------------------------------------------------------------------------------------
-
+# 3.5 Quintil NSE Barrio x Class ----------------------------------------------------------------------------------------------------------
+# Function for crosstable
 crosstable <- function(data, group1, group2, count_name) {
     data %>%
         group_by({{ group1 }}, {{ group2 }}) %>%
@@ -194,4 +169,5 @@ quint_nse_barrio_class_w06 <- crosstable(elsocs[[3]] %>% mutate(class_5 = to_lab
 quint_nse_barrio_class <- bind_cols(list(quint_nse_barrio_class_w01, quint_nse_barrio_class_w04, quint_nse_barrio_class_w06))
 
 # Save!
-writexl::write_xlsx(quint_nse_barrio_class, "output/tables/quint_nse_barrio_class_tab.xlsx")
+writexl::write_xlsx(quint_nse_barrio_class, glue("output/tables/{date}_quint_nse_barrio_class_tab.xlsx"))
+rm(quint_nse_barrio_class, quint_nse_barrio_class_w01, quint_nse_barrio_class_w04, quint_nse_barrio_class_w06, crosstable)
