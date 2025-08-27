fa7439d79690cac007ddae06a2139d45e8eb87ee
Martin-Venegas-M <mvenegasm@ine.gob.cl>
2025-08-26 22:27:58 -0400

    drop na's post proc

--- processing/4_create_vars.R
+++ processing/4_create_vars.R
@@ -249,7 +249,7 @@ elsocs <- map(
     elsocs,
     .f = function(x) {
         x %>% select(
-            idencuesta, ola, geocodigo, fact_exp02, segmento, region, region_cod, yr_address,
+            idencuesta, ola, geocodigo, fact_exp02, segmento, region, region_cod,
             identification:justif_violence,
             z_identification:z_justif_violence,
             class, class_8, class_5,
@@ -259,3 +259,23 @@ elsocs <- map(
         )
     }
 )
+
+# 4.9 Drop NA -------------------------------------------------------------------------------------------------------------------------------------------------
+
+# Save missing plots
+visdat::vis_miss(elsocs[[1]])
+ggsave("output/plots/miss_elsoc_2016.png")
+
+visdat::vis_miss(elsocs[[2]])
+ggsave("output/plots/miss_elsoc_2019.png")
+
+visdat::vis_miss(elsocs[[3]])
+ggsave("output/plots/miss_elsoc_2022.png")
+
+# Drop na's!
+elsocs <- map(elsocs, .f = function(x) x %>% drop_na())
+
+#* When droping NA:
+#* elsoc_2016 removed 275 rows (15%), 1,613 rows remaining
+#* elsoc_2019 removed 155 rows (7%), 2,101 rows remaining
+#* elsoc_2022 removed 132 rows (7%), 1,668 rows remaining
