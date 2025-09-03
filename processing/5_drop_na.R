42dbff49a6b0a526b48e5a3eaebf6f2e1529dba8
Martin-Venegas-M <mvenegasm@ine.gob.cl>
2025-09-03 14:00:24 -0400

    join new_class with 5 cluster

--- processing/5_drop_na.R
+++ processing/5_drop_na.R
@@ -1,14 +1,14 @@
 # 5.1 Drop NA -------------------------------------------------------------------------------------------------------------------------------------------------
 
 # Save missing plots
-visdat::vis_miss(elsocs[[1]])
-ggsave("output/plots/miss_elsoc_2016.png")
+# visdat::vis_miss(elsocs[[1]])
+# ggsave("output/plots/miss_elsoc_2016.png")
 
-visdat::vis_miss(elsocs[[2]])
-ggsave("output/plots/miss_elsoc_2019.png")
+# visdat::vis_miss(elsocs[[2]])
+# ggsave("output/plots/miss_elsoc_2019.png")
 
-visdat::vis_miss(elsocs[[3]])
-ggsave("output/plots/miss_elsoc_2022.png")
+# visdat::vis_miss(elsocs[[3]])
+# ggsave("output/plots/miss_elsoc_2022.png")
 
 # Drop na's!
 elsocs <- map(elsocs, .f = function(x) x %>% drop_na())
@@ -18,4 +18,4 @@ elsocs <- map(elsocs, .f = function(x) x %>% drop_na())
 #* elsoc_2019 removed 155 rows (7%), 2,101 rows remaining
 #* elsoc_2022 removed 132 rows (7%), 1,668 rows remaining
 
-#! REVISAR SI HAY ALGUN PATRÓN POR CIUDAD EN LA ELIMINACIÓN DE NA
+# ! REVISAR SI HAY ALGUN PATRÓN POR CIUDAD EN LA ELIMINACIÓN DE NA
--- processing/6_join_new_class.R
+++ processing/6_join_new_class.R
@@ -1,10 +1,10 @@
 # 6.1 Join new class variables ------------------------------------------------------------------------------------
 
 # Load model results
-load("output/models/250903_results_mca_hcpc.RData")
+load("output/models/250904_results_mca_hcpc.RData")
 
 # Save dataset with relevant information in list
-datas <- map(1:3, ~ (results_all[[.x]]$class6$data %>% select(idencuesta, class_index = acm_scores1, new_class = clusters_6)))
+datas <- map(1:3, ~ (results_all[[.x]]$class5$data %>% select(idencuesta, class_index = acm_scores1, new_class = clusters_5)))
 
 # Join vars
 elsocs <- map2(elsocs, datas, ~ (.x %>% left_join(.y, by = "idencuesta")))
