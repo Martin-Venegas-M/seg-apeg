af263bd5b0f93cc31e33d9f40a6ec4b3ef07fe34
Martin-Venegas-M <mvenegasm@ine.gob.cl>
2025-11-10 14:50:29 -0300

    plot preds

--- analysis/plot-preds.R
+++ analysis/plot-preds.R
@@ -59,11 +59,11 @@ plotpreds <- function(
     model <- results_mm[[data_year]][[vardep]][[model]]
 
     # Make predictions
-    marginales <- ggpredict(model, terms = terms) %>%
+    preds <- ggpredict(model, terms = terms) %>%
         filter(group %in% cats.categorical.term)
 
     # Create plot
-    plot <- ggplot(marginales, aes(x = x, y = predicted, color = group)) +
+    plot <- ggplot(preds, aes(x = x, y = predicted, color = group)) +
         geom_line(size = 1.2) +
         geom_ribbon(aes(ymin = conf.low, ymax = conf.high, fill = group), alpha = 0.15, color = NA) +
         labs(
@@ -110,10 +110,34 @@ final_plot <- function(plot_list) {
 
 # 3.2 Iterate! ------------------------------------------------------------------------------------------------------------------------------------------
 
+sig_2016 <- c(
+    "identification", # Class 5
+    "trust_inst", # Class 5
+    "satisf_demo" # Class 5
+    # "unconv_particip", # Class 4
+    # "prosoc_behave" # Class 1
+)
+
+sig_2019 <- c(
+    "trust_inst" # Class 5
+)
+
+sig_2022 <- c(
+    "trust_inst", # Class 4 y 5
+    "interest_pol", # Class 5
+    "unconv_particip" # Class 5
+)
+
 preds <- list(
-    elsoc_2016 = map2(names(vardep_labels), vardep_labels, ~ plotpreds("elsoc_2016", .x, .y)),
-    elsoc_2019 = map2(names(vardep_labels), vardep_labels, ~ plotpreds("elsoc_2019", .x, .y)),
-    elsoc_2022 = map2(names(vardep_labels), vardep_labels, ~ plotpreds("elsoc_2022", .x, .y))
+    elsoc_2016 = map2(sig_2016, vardep_labels[names(vardep_labels) %in% sig_2016], ~ plotpreds("elsoc_2016", .x, .y)),
+    elsoc_2019 = map2(sig_2019, vardep_labels[names(vardep_labels) %in% sig_2019], ~ plotpreds("elsoc_2019", .x, .y)),
+    elsoc_2022 = map2(sig_2022, vardep_labels[names(vardep_labels) %in% sig_2022], ~ plotpreds("elsoc_2022", .x, .y))
+)
+
+preds_trust_inst <- list(
+    elsoc_2016 = plotpreds("elsoc_2016", "trust_inst", "Institutional trust - 2016"),
+    elsoc_2019 = plotpreds("elsoc_2019", "trust_inst", "Institutional trust - 2019"),
+    elsoc_2022 = plotpreds("elsoc_2022", "trust_inst", "Institutional trust - 2022")
 )
 
 # 4. Save ------------------------------------------------------------------------------------------------------------------------------------------------
@@ -125,4 +149,11 @@ map(
         width = 12, height = 12, dpi = 300,
         device = ragg::agg_png
     )
+)
+
+ggsave(
+    filename = glue("output/plots/preds_trust_inst.png"),
+    plot = final_plot(preds_trust_inst),
+    width = 12, height = 12, dpi = 300,
+    device = ragg::agg_png
 )
