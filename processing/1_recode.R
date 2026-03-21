6a23a972332ed091e33e056a08387a6303f8e032
Martin-Venegas-M <martin.venegas@ug.uchile.cl>
2026-03-21 15:37:04 -0300

    improve code structure

--- processing/1_recode.R
+++ processing/1_recode.R
@@ -8,16 +8,12 @@
 #! [THIS SCRIPT IS MEANT TO BE RUN VIA THE run_processing.R SCRIPT]
 
 # 1.1 Select variables --------------------------------------------------------
-elsoc <- elsoc_original %>%
+elsoc <- elsoc_original |>
   select(
     # Survey variables
-    starts_with("idencuesta"),
-    starts_with("ola"),
-    starts_with("muestra"),
-    starts_with("estrato"),
+    starts_with(c("idencuesta", "ola", "muestra", "estrato")),
     # 1. Sense of belonging and identification
-    starts_with("c32_01"),
-    starts_with("c32_02"),
+    starts_with(c("c32_01", "c32_02")),
     # 2. Number of friends
     starts_with("r15"),
     # 3. Intimate network size
@@ -25,87 +21,55 @@ elsoc <- elsoc_original %>%
     # 4. Generalized trust
     starts_with("c02"),
     # 5. Trust in social minorities
-    starts_with("c06_04"),
-    starts_with("c06_05"),
-    starts_with("c06_06"),
+    starts_with(c("c06_04", "c06_05", "c06_06")),
     # 6. Trust in major institutions
-    starts_with("c05_01"),
-    starts_with("c05_02"),
-    starts_with("c05_05"),
-    starts_with("c05_07"),
+    starts_with(c("c05_01", "c05_02", "c05_05", "c05_07")),
     # 7. Interest in political affairs
     starts_with("c13"),
     # 8. Satisfaction with democracy
     starts_with("c01"),
     # 9. Conventional political participation
-    starts_with("c12_01"),
-    starts_with("c12_03"),
-    starts_with("c12_04"),
-    starts_with("c12_05"),
+    starts_with(c("c12_01", "c12_03", "c12_04", "c12_05")),
     # 10. Unconventional political participation
-    starts_with("c08_01"),
-    starts_with("c08_02"),
-    starts_with("c08_03"),
+    starts_with(c("c08_01", "c08_02", "c08_03")),
     # 11. Egalitarianism
-    starts_with("d02_01"),
-    starts_with("d02_02"),
-    starts_with("d02_03"),
+    starts_with(c("d02_01", "d02_02", "d02_03")),
     # 12. Altruistic disposition
-    starts_with("c18_02"),
-    starts_with("c18_03"),
+    starts_with(c("c18_02", "c18_03")),
     # 13. Pro-social behavior
-    starts_with("c07_04"),
-    starts_with("c07_05"),
+    starts_with(c("c07_04", "c07_05")),
     # 14. Support for democracy
     starts_with("c25"),
     # 15. Justification of violence
-    starts_with("f05_01"),
-    starts_with("f05_02"),
-    starts_with("f05_03"),
+    starts_with(c("f05_01", "f05_02", "f05_03")),
     # Other covariables
-    starts_with("m0_sexo"),
-    starts_with("m0_edad"),
-    starts_with("m01"),
-    starts_with("m02"),
-    starts_with("ciuo88_m03"),
-    starts_with("ciuo08_m03"),
-    starts_with("ciuo88_m22"),
-    starts_with("ciuo08_m22"),
-    starts_with("m19"),
-    starts_with("m20"),
-    starts_with("m21"),
-    starts_with("m29"),
-    starts_with("m33"),
-    starts_with("m34"),
-    starts_with("m36"),
-    starts_with("m37"), # Control variables
-
-    starts_with("fact_exp02"),
-    starts_with("segmento"),
-    starts_with("region") # Other variables
-  ) %>%
+    starts_with(c("m0_sexo", "m0_edad", "m01", "m02")),
+    starts_with(c("ciuo88_m03", "ciuo08_m03", "ciuo88_m22", "ciuo08_m22")),
+    starts_with(c("m19", "m20", "m21", "m29", "m33", "m34", "m36", "m37")),
+    starts_with(c("fact_exp02", "segmento", "region"))
+  ) |>
   select(
     -starts_with("m33_otro"),
     -starts_with("m36_otro"),
     -starts_with("idencuestador")
-  ) %>%
+  ) |>
   mutate(
-    across(everything(), ~ if_else(. %in% c(-666, -777, -888, -999), NA, .)), # ! RECODE TO NA SPECIAL VALUES (TECHNICAL ERRORS, NON RESPONSE ETC.)
-    across(everything(), ~ remove_value_labels(., c(-666, -777, -888, -999))) # ! DROPS LABELS FROM SPECIAL VALUES
+    # ! RECODE TO NA SPECIAL VALUES (TECHNICAL ERRORS, NON RESPONSE ETC.)
+    across(everything(), ~ if_else(. %in% c(-666, -777, -888, -999), NA, .)),
+    # ! DROPS LABELS FROM SPECIAL VALUES
+    across(everything(), ~ remove_value_labels(., c(-666, -777, -888, -999)))
   )
 
 # 1.2 Recode specific variables -----------------------------------------------
 
-elsoc <- elsoc %>%
-  # Generalized trust: recode in order to create an ordinal variable
-  # (+gen_trust -> +atachment to society)
+elsoc <- elsoc |>
   mutate(
-    # Recoding values
+    # Generalized trust: recode in order to create an ordinal variable
+    # (+gen_trust -> +atachment to society)
     across(
       starts_with("c02"),
       ~ case_when(. == 1 ~ 3, . == 3 ~ 2, . == 2 ~ 1, TRUE ~ .)
     ),
-    # Recoding labels
     across(
       starts_with("c02"),
       ~ set_labels(
@@ -116,13 +80,11 @@ elsoc <- elsoc %>%
           "Casi siempre se puede confiar en las personas" = 3
         )
       )
-    )
-  ) %>%
-  # Justifiction of violence: invert the scale in order to
-  #  +justif_violence -> +atachment to society
-  mutate(
-    across(starts_with("f05"), ~ invert_scale(.)), # Recoding values
-    # Recoding labels
+    ),
+
+    # Justification of violence: invert the scale (
+    # +justif_violence -> +atachment to society)
+    across(starts_with("f05"), ~ invert_scale(.)),
     across(
       starts_with("f05"),
       ~ set_labels(
@@ -135,32 +97,28 @@ elsoc <- elsoc %>%
           "Nunca se justifica" = 5
         )
       )
-    )
-  ) %>%
-  # Size network: create binary variable
-  mutate(
+    ),
+
+    # Network size: create binary variable (below/above median)
     across(
       starts_with("r13"),
       ~ if_else(. >= median(., na.rm = T), 1, 0),
       .names = "rec_{.col}"
-    ), # Recoding values
+    ),
     across(
       starts_with("_rec"),
       ~ set_labels(
         .,
         labels = c("Below the median" = 0, "Equal to or above the median" = 1)
       )
-    ) # Recoding labels
-  ) %>%
-  # Democracy support: recode the scale in order to
-  #  +democracy support -> +atachment to society
-  mutate(
-    # Recoding values
+    ),
+
+    # Democracy support: recode the scale 
+    # (+democracy support -> +atachment to society)
     across(
       starts_with("c25"),
       ~ case_when(. == 2 ~ 1, . %in% c(3, 4) ~ 2, . == 1 ~ 3)
     ),
-    # Recoding labels
     across(
       starts_with("c25"),
       ~ set_labels(
@@ -171,12 +129,10 @@ elsoc <- elsoc %>%
           "La democracia es preferible a cualquier otra forma de gobierno" = 3
         )
       )
-    )
-  ) %>%
-  # Egalitarianism: invert the scale in order to +egalitarianism -> + atachment to society
-  mutate(
-    across(starts_with("d02"), ~ invert_scale(.)), # Recoding values
-    # Recoding labels
+    ),
+
+    # Egalitarianism: invert the scale (+egalitarianism -> +atachment to society)
+    across(starts_with("d02"), ~ invert_scale(.)),
     across(
       starts_with("d02"),
       ~ set_labels(
--- processing/run_processing.R
+++ processing/run_processing.R
@@ -1,9 +1,11 @@
 #******************************************************************************
 # 0. Identification -----------------------------------------------------------
-# Title: Processing code for a research paper on Residential segregation ans Attachment to society - Crossectional analysis
+# Title: Processing code for a research paper on Residential segregation and
+# Attachment to society - Crossectional analysis
 # Institution: Centro de Estudios de Conflicto y Cohesión Social (COES)
 # Responsable: Technical assistant
-# Executive Summary: This script contains the code to run all parts of the data processing process.
+# Executive Summary: This script contains the code to run all parts of the data
+#  processing process.
 # Date: August 18, 2025
 #******************************************************************************
 
@@ -14,36 +16,30 @@ if (!require("pacman")) {
   install.packages("pacman")
 } # if pacman es missing, install
 
-pacman::p_load(
-  tidyverse,
-  haven,
-  tidylog,
-  rlang,
-  sjlabelled,
-  RStata,
-  ISCO08ConveRsions,
-  visdat
-)
+pacman::p_load(tidyverse, haven, tidylog, rlang, sjlabelled, visdat)
 
 # 2. Load data ----------------------------------------------------------------
 elsoc_original <- haven::read_stata(
   "input/data/original/ELSOC_Wide_2016_2022.dta"
 ) # This is the original data from ELSOC webpage
 
-insumo_ciuo <- readxl::read_xlsx("input/insumo_ciuo.xlsx") # Source document with the equivalences between ciuo88 and ciuo08 (ciuo = isco)
+# Source document with the equivalences between ciuo88 and ciuo08 (ciuo = isco)
+insumo_ciuo <- readxl::read_xlsx("input/insumo_ciuo.xlsx")
 
+# Source document with neighbourhood variables and geocodigo
 insumo_barrio <- read_csv(
   "input/data/pre-proc/muestra_nacional_nse_barrio.csv"
-) %>%
-  as_tibble() %>%
-  select(-1) # Source document with neighbourhood variables and geocodigo
+) |>
+  as_tibble() |>
+  select(-1)
 
+# Source document with equivalences between isco and oesch class scheme
 insumo_oesch <- readxl::read_excel(
   "input/Final_proposition_passage_ISCO08_Oesch_10_06_2014.xls"
-) %>% # Source document with equivalences between isco and oesch class scheme
-  select("isco" = 1, "description" = 2, "class" = 3) %>%
-  mutate(isco = as.numeric(isco)) %>%
-  select(-description) %>%
+) |>
+  select("isco" = 1, "description" = 2, "class" = 3) |>
+  mutate(isco = as.numeric(isco)) |>
+  select(-description) |>
   mutate(class = as.numeric(if_else(class == "leave aside", NA, class)))
 
 source("processing/helpers/functions.R", encoding = "UTF-8") # Utility functions for processing data
